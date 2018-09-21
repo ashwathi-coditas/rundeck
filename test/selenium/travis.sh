26d229ceeae60ddc90f90a32d9941da4f69755d6
ProTip <greg.zapp@gmail.com>
2018-09-21 15:36:45 -0500

    Do not skip sync

--- test/selenium/travis.sh
+++ test/selenium/travis.sh
@@ -6,12 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 main() {
     S3_BASE="projects/rundeck/branch/${RUNDECK_BRANCH}/build/${RUNDECK_BUILD_NUMBER}/selenium"
 
+    RET=0
     docker-compose run \
         -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-        selenium "npm install && ./bin/deck selenium -u http://rundeck:4440 -h --s3-upload --s3-base ${S3_BASE}"
-
-    RET=$?
+        selenium "npm install && ./bin/deck selenium -u http://rundeck:4440 -h --s3-upload --s3-base ${S3_BASE}" || RET=$?
 
     aws s3 sync __image_snapshots__/__diff_output__ s3://ci.rundeck.org/$S3_BASE/diff
 
