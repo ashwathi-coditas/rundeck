00375eb65ced19b04bf42205b9291abdefb038cc
ProTip <greg.zapp@gmail.com>
2018-09-22 16:03:32 -0500

    In case everything passes..

--- test/selenium/travis.sh
+++ test/selenium/travis.sh
@@ -12,7 +12,11 @@ main() {
         -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
         selenium "npm install && ./bin/deck selenium -u http://rundeck:4440 -h --s3-upload --s3-base ${S3_BASE}" || RET=$?
 
-    aws s3 sync __image_snapshots__/__diff_output__ s3://ci.rundeck.org/$S3_BASE/diff
+    local DIFF_DIR=__image_snapshots__/__diff_output__
+
+    if [[ -d "${DIFF_DIR}" ]] ; then
+        aws s3 sync $DIFF_DIR s3://ci.rundeck.org/$S3_BASE/diff
+    fi
 
     echo "Image output available at: http://ci.rundeck.com/$S3_BASE"
 
